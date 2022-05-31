24e85dfba4124d5410caa22371d6ac928ba73064
akashpd <akash.prasad1@gmail.com>
2022-06-01 02:08:33 +0530

    added new ml model 3 code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
 print("adding all ML related code")
+
+
+def model1():
+    prin.t("addin code for ML model 1")
+
+
+def model2():
+    prin.t("addin code for ML model 2")
+
+
+
+def model3():
+    prin.t("addin code for ML model 3")
